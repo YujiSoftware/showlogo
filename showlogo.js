61acc95db82c25ff4de750fb2ddc5f0208cfab8a
YujiSoftware <yuji.software+github@gmail.com>
2015-11-05 02:07:42 +0900

    Modify rendering position

--- showlogo.js
+++ showlogo.js
@@ -5,9 +5,13 @@ function wave(){
   
   ctx.fillStyle="#000000";
   ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+  // 出力先座標 (キャンバスの中央)
+  var dx = (canvas.clientWidth - src.naturalWidth) / 2;
+  var dy = (canvas.clientHeight - src.naturalHeight) / 2;
   
   for(var i = 0; i < src.naturalHeight; i++){
-    ctx.drawImage(src, 0, i, src.naturalWidth, 1, 15 * Math.sin((i * 10) * Math.PI / 180), i, src.naturalWidth, 1);
+    ctx.drawImage(src, 0, i, src.naturalWidth, 1, dx + 15 * Math.sin((i * 10) * Math.PI / 180), dy + i, src.naturalWidth, 1);
   }
 }
 
