0ba7b90ffaaec13c2bd5e801ddedeb1bf968ee77
YujiSoftware <yuji.software+github@gmail.com>
2015-11-10 02:06:33 +0900

    Fixed verticalWave

--- showlogo.js
+++ showlogo.js
@@ -22,6 +22,8 @@ function verticalWave(){
   var canvas = document.getElementById('dest');
   var src = document.getElementById('src');
   var ctx = canvas.getContext('2d');
+
+  reset();
   
   // 出力先座標 (キャンバスの中央)
   var dx = (canvas.clientWidth - src.naturalWidth) / 2;
@@ -30,8 +32,8 @@ function verticalWave(){
   var elapsedTime = new Date().getTime() - startTime;
   var pitch = (elapsedTime / 5) % 360;
   
-  for(var i = 0; i < src.naturalWidth; i++){
-    ctx.drawImage(src, i, 0, 1, src.naturalHeight, dx + i, dy + 15 * Math.sin((i * 10 + pitch) * Math.PI / 180), 1, src.naturalHeight);
+  for(var i = 0; i < src.naturalHeight; i += 0.3){
+    ctx.drawImage(src, 0, i, src.naturalWidth, 1, dx, dy + i + 10 * Math.sin((i * 10 + pitch) * Math.PI / 180), src.naturalWidth, 1);
   }
 }
 
