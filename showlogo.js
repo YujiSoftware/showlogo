e5ac542dcb573ea7354b9c31936f41687dfde8f9
YujiSoftware <yuji.software+github@gmail.com>
2015-11-13 02:19:38 +0900

    Implemented waterfall

--- showlogo.js
+++ showlogo.js
@@ -37,6 +37,31 @@ function verticalWave(){
   }
 }
 
+function waterfall(){
+  var canvas = document.getElementById('dest');
+  var src = document.getElementById('src');
+  var ctx = canvas.getContext('2d');
+
+  reset();
+  
+  // 出力先座標 (キャンバスの中央)
+  var dx = (canvas.clientWidth - src.naturalWidth) / 2;
+  var dy = (canvas.clientHeight - src.naturalHeight) / 2;
+  
+  var now = new Date().getTime();
+  var height = (now - startTime) / 30;
+  
+  if(src.naturalHeight <= height){
+    clearInterval(intervalId);
+    height = src.naturalHeight;
+  }
+
+  // 伸びない部分の描画
+  ctx.drawImage(src, 0, src.naturalHeight - height, src.naturalWidth, height, dx, dy + src.naturalHeight - height, src.naturalWidth, height);
+  // 伸びる部分の描画
+  ctx.drawImage(src, 0, src.naturalHeight - height, src.naturalWidth, 1, dx, 0, src.naturalWidth, dy + src.naturalHeight - height);
+}
+
 function reset(){
   var canvas = document.getElementById('dest');
   var src = document.getElementById('src');
