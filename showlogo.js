34f0221a730e022f463936925573246914c5fb00
YujiSoftware <yuji.software+github@gmail.com>
2015-11-22 23:11:07 +0900

    Refactord src and dest variable

--- showlogo.js
+++ showlogo.js
@@ -1,11 +1,22 @@
 var ShowLogo = function(){
+  this.initialize.apply(this, arguments);
 };
 
 ShowLogo.prototype = {
   startTime: 0,
   intervalId: 0,
+  canvas: null,
+  ctx: null,
+  src: null,
 
-  start: function(func){
+  initialize: function(canvas){
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d");
+  },
+
+  start: function(func, src){
+    this.src = src;
+  
     this.stop();
     
     this.startTime = new Date().getTime();
@@ -18,99 +29,79 @@ ShowLogo.prototype = {
   },
 
   horizontalWave: function(){
-    var canvas = document.getElementById('dest');
-    var src = document.getElementById('src');
-    var ctx = canvas.getContext('2d');
-    
     // 出力先座標 (キャンバスの中央)
-    var dx = (canvas.clientWidth - src.naturalWidth) / 2;
-    var dy = (canvas.clientHeight - src.naturalHeight) / 2;
+    var dx = (this.canvas.clientWidth - this.src.naturalWidth) / 2;
+    var dy = (this.canvas.clientHeight - this.src.naturalHeight) / 2;
     
     var elapsedTime = new Date().getTime() - this.startTime;
     var pitch = (elapsedTime / 5) % 360;
     
-    for(var i = 0; i < src.naturalHeight; i++){
-      ctx.drawImage(src, 0, i, src.naturalWidth, 1, dx + 15 * Math.sin((i * 10 + pitch) * Math.PI / 180), dy + i, src.naturalWidth, 1);
+    for(var i = 0; i < this.src.naturalHeight; i++){
+      this.ctx.drawImage(this.src, 0, i, this.src.naturalWidth, 1, dx + 15 * Math.sin((i * 10 + pitch) * Math.PI / 180), dy + i, this.src.naturalWidth, 1);
     }
   },
 
   verticalWave: function(){
-    var canvas = document.getElementById('dest');
-    var src = document.getElementById('src');
-    var ctx = canvas.getContext('2d');
-
     this.reset();
     
     // 出力先座標 (キャンバスの中央)
-    var dx = (canvas.clientWidth - src.naturalWidth) / 2;
-    var dy = (canvas.clientHeight - src.naturalHeight) / 2;
+    var dx = (this.canvas.clientWidth - this.src.naturalWidth) / 2;
+    var dy = (this.canvas.clientHeight - this.src.naturalHeight) / 2;
     
     var elapsedTime = new Date().getTime() - this.startTime;
     var pitch = (elapsedTime / 5) % 360;
     
-    for(var i = 0; i < src.naturalHeight; i += 0.3){
-      ctx.drawImage(src, 0, i, src.naturalWidth, 1, dx, dy + i + 10 * Math.sin((i * 10 + pitch) * Math.PI / 180), src.naturalWidth, 1);
+    for(var i = 0; i < this.src.naturalHeight; i += 0.3){
+      this.ctx.drawImage(this.src, 0, i, this.src.naturalWidth, 1, dx, dy + i + 10 * Math.sin((i * 10 + pitch) * Math.PI / 180), this.src.naturalWidth, 1);
     }
   },
 
   waterfall: function(){
-    var canvas = document.getElementById('dest');
-    var src = document.getElementById('src');
-    var ctx = canvas.getContext('2d');
-
     this.reset();
     
     // 出力先座標 (キャンバスの中央)
-    var dx = (canvas.clientWidth - src.naturalWidth) / 2;
-    var dy = (canvas.clientHeight - src.naturalHeight) / 2;
+    var dx = (this.canvas.clientWidth - this.src.naturalWidth) / 2;
+    var dy = (this.canvas.clientHeight - this.src.naturalHeight) / 2;
     
     var now = new Date().getTime();
     var height = (now - this.startTime) / 30;
     
-    if(src.naturalHeight <= height){
+    if(this.src.naturalHeight <= height){
       clearInterval(this.intervalId);
-      height = src.naturalHeight;
+      height = this.src.naturalHeight;
     }
 
     // 伸びない部分の描画
-    ctx.drawImage(src, 0, src.naturalHeight - height, src.naturalWidth, height, dx, dy + src.naturalHeight - height, src.naturalWidth, height);
+    this.ctx.drawImage(this.src, 0, this.src.naturalHeight - height, this.src.naturalWidth, height, dx, dy + this.src.naturalHeight - height, this.src.naturalWidth, height);
     // 伸びる部分の描画
-    ctx.drawImage(src, 0, src.naturalHeight - height, src.naturalWidth, 1, dx, 0, src.naturalWidth, dy + src.naturalHeight - height);
+    this.ctx.drawImage(this.src, 0, this.src.naturalHeight - height, this.src.naturalWidth, 1, dx, 0, this.src.naturalWidth, dy + this.src.naturalHeight - height);
   },
 
   slide: function(){
-    var canvas = document.getElementById('dest');
-    var src = document.getElementById('src');
-    var ctx = canvas.getContext('2d');
-
     this.reset();
     
     // 出力先座標 (キャンバスの中央)
-    var dx = (canvas.clientWidth - src.naturalWidth) / 2;
-    var dy = (canvas.clientHeight - src.naturalHeight) / 2;
+    var dx = (this.canvas.clientWidth - this.src.naturalWidth) / 2;
+    var dy = (this.canvas.clientHeight - this.src.naturalHeight) / 2;
     
     var now = new Date().getTime();
     var offset = (now - this.startTime) / 8;
     
-    if(dx + src.naturalWidth <= offset){
+    if(dx + this.src.naturalWidth <= offset){
       clearInterval(this.intervalId);
-      offset = dx + src.naturalWidth;
+      offset = dx + this.src.naturalWidth;
     }
 
-    for(var i = 0; i < src.naturalHeight; i += 2){
-      ctx.drawImage(src, 0, i, src.naturalWidth, 1, offset - src.naturalWidth, dy + i, src.naturalWidth, 1);
+    for(var i = 0; i < this.src.naturalHeight; i += 2){
+      this.ctx.drawImage(this.src, 0, i, this.src.naturalWidth, 1, offset - this.src.naturalWidth, dy + i, this.src.naturalWidth, 1);
     }
-    for(var i = 1; i < src.naturalHeight; i += 2){
-      ctx.drawImage(src, 0, i, src.naturalWidth, 1, canvas.clientWidth - offset, dy + i, src.naturalWidth, 1);
+    for(var i = 1; i < this.src.naturalHeight; i += 2){
+      this.ctx.drawImage(this.src, 0, i, this.src.naturalWidth, 1, this.canvas.clientWidth - offset, dy + i, this.src.naturalWidth, 1);
     }
   },
 
-  reset: function(){
-    var canvas = document.getElementById('dest');
-    var src = document.getElementById('src');
-    var ctx = canvas.getContext('2d');
-    
-    ctx.fillStyle="#000000";
-    ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+  reset: function(){    
+    this.ctx.fillStyle="#000000";
+    this.ctx.fillRect(0, 0, this.canvas.clientWidth, this.canvas.clientHeight);
   }
 };
