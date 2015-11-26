f08ecd5bc05516b6ffb7c3ad58fa84687b25458a
YujiSoftware <yuji.software+github@gmail.com>
2015-11-27 01:56:28 +0900

    Implemented zoomOut

--- showlogo.js
+++ showlogo.js
@@ -126,6 +126,25 @@ ShowLogo.prototype = {
     }
   },
 
+  zoomOut: function(){
+    this.reset();
+
+    // 出力先座標 (キャンバスの中央)
+    var dx = (this.canvas.clientWidth - this.src.naturalWidth) / 2;
+    var dy = (this.canvas.clientHeight - this.src.naturalHeight) / 2;
+
+    var now = new Date().getTime();
+    var elapsedTime = now - this.startTime;
+    var time = 1000;
+    
+    if(time <= elapsedTime){
+      clearInterval(this.intervalId);
+      elapsedTime = time;
+    }
+
+    this.ctx.drawImage(this.src, 0, 0, this.src.naturalWidth, this.src.naturalHeight, elapsedTime / 20 * 1.4, elapsedTime / 20, this.src.naturalWidth * 2 - elapsedTime / 20 * 2.8, this.src.naturalHeight * 2 - elapsedTime / 20 * 2);
+  },
+
   reset: function(){    
     this.ctx.fillStyle="#000000";
     this.ctx.fillRect(0, 0, this.canvas.clientWidth, this.canvas.clientHeight);
