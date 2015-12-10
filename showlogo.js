da491f4dcd67358a0f9bc4a7685a07a36da269ca
YujiSoftware <yuji.software+github@gmail.com>
2015-12-11 01:46:20 +0900

    Refactored position calculation

--- showlogo.js
+++ showlogo.js
@@ -6,6 +6,7 @@ ShowLogo.prototype = {
   startTime: 0,
   intervalId: 0,
   canvas: null,
+  position: null,
   ctx: null,
   src: null,
 
@@ -16,6 +17,12 @@ ShowLogo.prototype = {
 
   start: function(func, src){
     this.src = src;
+
+    // 出力先座標
+    this.position = {
+      dx: (this.canvas.clientWidth - this.src.naturalWidth) / 2,
+      dy: (this.canvas.clientHeight - this.src.naturalHeight) / 2
+    };
   
     this.stop();
     
@@ -31,39 +38,27 @@ ShowLogo.prototype = {
   horizontalWave: function(){
     this.reset();
     
-    // 出力先座標 (キャンバスの中央)
-    var dx = (this.canvas.clientWidth - this.src.naturalWidth) / 2;
-    var dy = (this.canvas.clientHeight - this.src.naturalHeight) / 2;
-    
     var elapsedTime = new Date().getTime() - this.startTime;
     var pitch = (elapsedTime / 5) % 360;
     
     for(var i = 0; i < this.src.naturalHeight; i++){
-      this.ctx.drawImage(this.src, 0, i, this.src.naturalWidth, 1, dx + 15 * Math.sin((i * 10 + pitch) * Math.PI / 180), dy + i, this.src.naturalWidth, 1);
+      this.ctx.drawImage(this.src, 0, i, this.src.naturalWidth, 1, this.position.dx + 15 * Math.sin((i * 10 + pitch) * Math.PI / 180), this.position.dy + i, this.src.naturalWidth, 1);
     }
   },
 
   verticalWave: function(){
     this.reset();
     
-    // 出力先座標 (キャンバスの中央)
-    var dx = (this.canvas.clientWidth - this.src.naturalWidth) / 2;
-    var dy = (this.canvas.clientHeight - this.src.naturalHeight) / 2;
-    
     var elapsedTime = new Date().getTime() - this.startTime;
     var pitch = (elapsedTime / 5) % 360;
     
     for(var i = 0; i < this.src.naturalHeight; i += 0.3){
-      this.ctx.drawImage(this.src, 0, i, this.src.naturalWidth, 1, dx, dy + i + 10 * Math.sin((i * 10 + pitch) * Math.PI / 180), this.src.naturalWidth, 1);
+      this.ctx.drawImage(this.src, 0, i, this.src.naturalWidth, 1, this.position.dx, this.position.dy + i + 10 * Math.sin((i * 10 + pitch) * Math.PI / 180), this.src.naturalWidth, 1);
     }
   },
 
   waterfall: function(){
     this.reset();
-    
-    // 出力先座標 (キャンバスの中央)
-    var dx = (this.canvas.clientWidth - this.src.naturalWidth) / 2;
-    var dy = (this.canvas.clientHeight - this.src.naturalHeight) / 2;
     
     var now = new Date().getTime();
     var height = (now - this.startTime) / 30;
@@ -74,40 +69,32 @@ ShowLogo.prototype = {
     }
 
     // 伸びない部分の描画
-    this.ctx.drawImage(this.src, 0, this.src.naturalHeight - height, this.src.naturalWidth, height, dx, dy + this.src.naturalHeight - height, this.src.naturalWidth, height);
+    this.ctx.drawImage(this.src, 0, this.src.naturalHeight - height, this.src.naturalWidth, height, this.position.dx, this.position.dy + this.src.naturalHeight - height, this.src.naturalWidth, height);
     // 伸びる部分の描画
-    this.ctx.drawImage(this.src, 0, this.src.naturalHeight - height, this.src.naturalWidth, 1, dx, 0, this.src.naturalWidth, dy + this.src.naturalHeight - height);
+    this.ctx.drawImage(this.src, 0, this.src.naturalHeight - height, this.src.naturalWidth, 1, this.position.dx, 0, this.src.naturalWidth, this.position.dy + this.src.naturalHeight - height);
   },
 
   slide: function(){
     this.reset();
     
-    // 出力先座標 (キャンバスの中央)
-    var dx = (this.canvas.clientWidth - this.src.naturalWidth) / 2;
-    var dy = (this.canvas.clientHeight - this.src.naturalHeight) / 2;
-    
     var now = new Date().getTime();
     var offset = (now - this.startTime) / 8;
     
-    if(dx + this.src.naturalWidth <= offset){
+    if(this.position.dx + this.src.naturalWidth <= offset){
       clearInterval(this.intervalId);
-      offset = dx + this.src.naturalWidth;
+      offset = this.position.dx + this.src.naturalWidth;
     }
 
     for(var i = 0; i < this.src.naturalHeight; i += 2){
-      this.ctx.drawImage(this.src, 0, i, this.src.naturalWidth, 1, offset - this.src.naturalWidth, dy + i, this.src.naturalWidth, 1);
+      this.ctx.drawImage(this.src, 0, i, this.src.naturalWidth, 1, offset - this.src.naturalWidth, this.position.dy + i, this.src.naturalWidth, 1);
     }
     for(var i = 1; i < this.src.naturalHeight; i += 2){
-      this.ctx.drawImage(this.src, 0, i, this.src.naturalWidth, 1, this.canvas.clientWidth - offset, dy + i, this.src.naturalWidth, 1);
+      this.ctx.drawImage(this.src, 0, i, this.src.naturalWidth, 1, this.canvas.clientWidth - offset, this.position.dy + i, this.src.naturalWidth, 1);
     }
   },
 
   slideAndWave: function(){
     this.reset();
-    
-    // 出力先座標 (キャンバスの中央)
-    var dx = (this.canvas.clientWidth - this.src.naturalWidth) / 2;
-    var dy = (this.canvas.clientHeight - this.src.naturalHeight) / 2;
     
     var now = new Date().getTime();
     var elapsedTime = now - this.startTime;
@@ -119,19 +106,15 @@ ShowLogo.prototype = {
     }
     
     for(var i = 0; i < this.src.naturalHeight; i += 2){
-      this.ctx.drawImage(this.src, 0, i, this.src.naturalWidth, 1, dx + ((time - elapsedTime) / 30) * Math.sin((i * 10 + (elapsedTime / 1.5)) * Math.PI / 180), dy + i, this.src.naturalWidth, 1);
+      this.ctx.drawImage(this.src, 0, i, this.src.naturalWidth, 1, this.position.dx + ((time - elapsedTime) / 30) * Math.sin((i * 10 + (elapsedTime / 1.5)) * Math.PI / 180), this.position.dy + i, this.src.naturalWidth, 1);
     }
     for(var i = 1; i < this.src.naturalHeight; i += 2){
-      this.ctx.drawImage(this.src, 0, i, this.src.naturalWidth, 1, dx + ((time - elapsedTime) / 30) * Math.sin(-(i * 10 + (elapsedTime / 1.5)) * Math.PI / 180), dy + i, this.src.naturalWidth, 1);
+      this.ctx.drawImage(this.src, 0, i, this.src.naturalWidth, 1, this.position.dx + ((time - elapsedTime) / 30) * Math.sin(-(i * 10 + (elapsedTime / 1.5)) * Math.PI / 180), this.position.dy + i, this.src.naturalWidth, 1);
     }
   },
 
   zoomOut: function(){
     this.reset();
-
-    // 出力先座標 (キャンバスの中央)
-    var dx = (this.canvas.clientWidth - this.src.naturalWidth) / 2;
-    var dy = (this.canvas.clientHeight - this.src.naturalHeight) / 2;
 
     var now = new Date().getTime();
     var elapsedTime = now - this.startTime;
@@ -147,10 +130,6 @@ ShowLogo.prototype = {
 
   zoomIn: function(){
     this.reset();
-
-    // 出力先座標 (キャンバスの中央)
-    var dx = (this.canvas.clientWidth - this.src.naturalWidth) / 2;
-    var dy = (this.canvas.clientHeight - this.src.naturalHeight) / 2;
 
     var now = new Date().getTime();
     var elapsedTime = now - this.startTime;
@@ -179,10 +158,6 @@ ShowLogo.prototype = {
       this.slideX.sort(function(a, b) { return random[a] - random[b]; });
     }
 
-    // 出力先座標 (キャンバスの中央)
-    var dx = (this.canvas.clientWidth - this.src.naturalWidth) / 2;
-    var dy = (this.canvas.clientHeight - this.src.naturalHeight) / 2;
-
     var now = new Date().getTime();
     var elapsedTime = now - this.startTime;
     var time = 1000;
@@ -190,7 +165,7 @@ ShowLogo.prototype = {
     var jotai = elapsedTime / time * this.src.naturalHeight;
 
     for(var i = 0; i < jotai; i++){
-      this.ctx.drawImage(this.src, 0, this.slideX[i], this.src.naturalWidth, 1, dx, dy + this.slideX[i], this.src.naturalWidth, 1);
+      this.ctx.drawImage(this.src, 0, this.slideX[i], this.src.naturalWidth, 1, this.position.dx, this.position.dy + this.slideX[i], this.src.naturalWidth, 1);
     }
     
     if(time <= elapsedTime){
